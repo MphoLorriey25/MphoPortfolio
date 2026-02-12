68ab2447000471626cb229ad03a358ec74d87acc
Mpho Lorraine Ndou <136015361+MphoLorriey25@users.noreply.github.com>
2026-02-12 11:56:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Trigger animations on scroll
+const animateItems = document.querySelectorAll('.animate');
+
+window.addEventListener('scroll', () => {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    animateItems.forEach(item => {
+        const itemTop = item.getBoundingClientRect().top;
+
+        if(itemTop < triggerBottom){
+            item.style.animationPlayState = 'running';
+        }
+    });
+});
 
